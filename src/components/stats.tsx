d8f1f6fb6d15d4e00deef4b7ed68583674cc4e90
Dhairya Joshi <77039476+TheRAWagent@users.noreply.github.com>
2024-06-18 18:32:19 +0530

    created stats component

---
+++ src/components/stats.tsx
@@ -0,0 +1,36 @@
+function Stats() {
+  return (
+    <div className="grid grid-flow-col auto-cols-fr md:auto-rows-fr">
+      <div className="p-6 gap-y-1 border-x w-80 bg-white flex flex-col items-start justify-center">
+        <div className="w-full flex flex-row justify-between items-center">
+          <div className="text-black">Revenue</div>
+          <div className="text-black">+4.75%</div>
+        </div>
+        <div className="text-2xl text-black">$405,091.00</div>
+      </div>
+      <div className="p-6 gap-y-1 border-x w-80 bg-white flex flex-col items-start justify-center">
+        <div className="w-full flex flex-row justify-between items-center">
+          <div className="text-black">Revenue</div>
+          <div className="text-black">+4.75%</div>
+        </div>
+        <div className="text-2xl text-black">$405,091.00</div>
+      </div>
+      <div className="p-6 gap-y-1 border-x w-80 bg-white flex flex-col items-start justify-center">
+        <div className="w-full flex flex-row justify-between items-center">
+          <div className="text-black">Revenue</div>
+          <div className="text-black">+4.75%</div>
+        </div>
+        <div className="text-2xl text-black">$405,091.00</div>
+      </div>
+      <div className="p-6 gap-y-1 border-x w-80 bg-white flex flex-col items-start justify-center">
+        <div className="w-full flex flex-row justify-between items-center">
+          <div className="text-black">Revenue</div>
+          <div className="text-black">+4.75%</div>
+        </div>
+        <div className="text-2xl text-black">$405,091.00</div>
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
